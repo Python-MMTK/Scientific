d3520e0301d219bbbecc39d06f7ec51e890d148a
xoviat <xoviat@users.noreply.github.com>
2017-12-03 20:44:11 -0600

    Python compatibility updates

--- Scientific/Functions/Polynomial.py
+++ Scientific/Functions/Polynomial.py
@@ -91,6 +91,8 @@ class Polynomial:
         return Polynomial(Numeric.sum(temp))
 
     def __div__(self, other):
+        if not hasattr(other, 'dim'):
+            other = Polynomial([other])
         if self.dim != 1 or other.dim != 1:
             raise ValueError("not implemented")
         if len(other.coeff) == 1:
