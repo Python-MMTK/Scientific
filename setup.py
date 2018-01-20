216389da1f87ee711b86961d4b617d4b77e3eccb
xoviat <xoviat@users.noreply.github.com>
2018-01-20 13:21:14 -0600

    Simply and improve setup

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
-from distutils.command.install_headers import install_headers
-import os, sys, platform
+import os
+import sys
+import platform
+import pkginfo
+import numpy.distutils.misc_util
+import textwrap
+
+from setuptools import setup
 from glob import glob
 
-class Dummy:
-    pass
-pkginfo = Dummy()
-execfile('Scientific/__pkginfo__.py', pkginfo.__dict__)
+from distutils.core import Extension
+from distutils.command.install_headers import install_headers
+from Cython.Build import cythonize
+from numpy.distutils.system_info import default_include_dirs, default_lib_dirs
 
-# Check for Cython and use it if the environment variable
-# COMPILE_CYTHON is set to a non-zero value.
-use_cython = int(os.environ.get('COMPILE_CYTHON', '0')) != 0
-if use_cython:
-    try:
-        from Cython.Build import cythonize
-        use_cython = True
-    except ImportError:
-        use_cython = False
-
-src_ext = 'pyx' if use_cython else 'c'
+use_cython = True
+src_ext = 'pyx'
 
 cmdclass = {}
 extra_compile_args = ["-DNUMPY=1"]
@@ -28,98 +24,24 @@ numpy_include = []
 data_files = []
 scripts = []
 options = {}
+is_py3 = False
 
-import numpy.distutils.misc_util
 numpy_include = numpy.distutils.misc_util.get_numpy_include_dirs()
 
 math_libraries = []
 if sys.platform != 'win32':
     math_libraries.append('m')
 
-#
-# Locate netCDF library
-#
-netcdf_prefix = None
-for arg in sys.argv[1:]:
-    if arg[:16] == "--netcdf_prefix=":
-        netcdf_prefix = arg[16:]
-        sys.argv.remove(arg)
-        break
 
-if sys.platform == 'win32':
-    netcdf_dll = None
-    for arg in sys.argv[1:]:
-        if arg[:13] == "--netcdf_dll=":
-            netcdf_dll = arg[13:]
-            sys.argv.remove(arg)
-            break
-
-if netcdf_prefix is None:
-    try:
-        netcdf_prefix=os.environ['NETCDF_PREFIX']
-    except KeyError:
-        pass
-if netcdf_prefix is None:
-    for netcdf_prefix in ['/usr/local', '/usr', '/sw']:
-        netcdf_include = os.path.join(netcdf_prefix, 'include')
-        netcdf_lib = os.path.join(netcdf_prefix, 'lib')
-        if os.path.exists(os.path.join(netcdf_include, 'netcdf.h')):
-            break
-    else:
-        netcdf_prefix = None
-
-if netcdf_prefix is None:
-    print "netCDF not found, the netCDF module will not be built!"
-    if sys.platform != 'win32':
-        print "If netCDF is installed somewhere on this computer,"
-        print "please set NETCDF_PREFIX to the path where"
-        print "include/netcdf.h and lib/netcdf.a are located"
-        print "and re-run the build procedure."
-    ext_modules = []
-else:
-    if sys.platform == 'win32':
-        if netcdf_dll is None:
-            print "Option --netcdf_dll is missing"
-            raise SystemExit
-        netcdf_include = netcdf_prefix
-        netcdf_h_file = os.path.join(netcdf_prefix, 'netcdf.h')
-        netcdf_lib = netcdf_dll
-        data_files.append(('DLLs', [os.path.join(netcdf_dll, 'netcdf.dll')]))
-        scripts.append('scientific_win32_postinstall.py')
-        options['bdist_wininst'] = {'install_script': "scientific_win32_postinstall.py"}
-    else:
-        print "Using netCDF installation in ", netcdf_prefix
-        netcdf_include = os.path.join(netcdf_prefix, 'include')
-        netcdf_h_file = os.path.join(netcdf_prefix, 'include', 'netcdf.h')
-        netcdf_lib = os.path.join(netcdf_prefix, 'lib')
-    ext_modules = [Extension('Scientific._netcdf',
-                             ['Scientific/_netcdf.c'],
-                             include_dirs=['Include', netcdf_include]
-                                          + numpy_include,
-                             library_dirs=[netcdf_lib],
-                             libraries = ['netcdf'],
-                             extra_compile_args=extra_compile_args)]
-
-try:
-    # Add code for including documentation in Mac packages
-    import bdist_mpkg
-    from distutils.command.bdist_mpkg import bdist_mpkg as bdist_mpkg
-    class my_bdist_mpkg(bdist_mpkg):
-        def initialize_options(self):
-            bdist_mpkg.initialize_options(self)
-
-            self.scheme_descriptions['examples'] = u'(Optional) ScientificPython example code'
-            self.scheme_map['examples'] = '/Developer/Python/ScientificPython/Examples'
-            self.scheme_copy['examples'] = 'Examples'
-
-            self.scheme_descriptions['doc'] = u'(Optional) ScientificPython documentation'
-            self.scheme_map['doc'] = '/Developer/Python/ScientificPython/Documentation'
-            self.scheme_copy['doc'] = 'Doc'
-
-    cmdclass['bdist_mpkg'] = my_bdist_mpkg
-
-except ImportError:
-    pass
+ext_modules = [Extension('Scientific._netcdf',
+                            ['Scientific/_netcdf.c'],
+                            include_dirs=(
+                                numpy_include + default_include_dirs +
+                                ['Include']
+                            ),
+                            library_dirs=default_lib_dirs,
+                            libraries = ['netcdf'],
+                            extra_compile_args=extra_compile_args)]
 
 packages = ['Scientific', 'Scientific.Clustering', 'Scientific.Functions',
             'Scientific.Geometry', 'Scientific.IO',
@@ -145,29 +67,16 @@ ext_modules.append(Extension('Scientific._interpolation',
                              libraries=math_libraries,
                              extra_compile_args=extra_compile_args))
 
-if use_cython:
-    ext_modules = cythonize(ext_modules)
-
 scripts.append('task_manager')
 if sys.version[:3] >= '2.1':
     packages.append('Scientific.BSP')
     scripts.append('bsp_virtual')
 
-class modified_install_headers(install_headers):
-
-    def finalize_options(self):
-        install_headers.finalize_options(self)
-        self.install_dir = \
-                os.path.join(os.path.split(self.install_dir)[0], 'Scientific')
-
-cmdclass['install_headers'] = modified_install_headers
-
 headers = glob(os.path.join ("Include","Scientific","*.h"))
-if netcdf_prefix is not None:
-    headers.append(netcdf_h_file)
+# headers.append(netcdf_h_file)
 
 setup (name = "ScientificPython",
-       version = pkginfo.__version__,
+       version = "2.6.2",
        description = "Various Python modules for scientific computing",
        long_description = 
 """ScientificPython is a collection of Python modules that are useful
@@ -184,11 +93,15 @@ line plots and 3D wireframe models.""",
        license = "CeCILL-C",
 
        packages = packages,
-       headers = headers,
-       ext_modules = ext_modules,
+       ext_modules = cythonize(ext_modules),
        scripts = scripts,
        data_files = data_files,
- 
+       headers = headers,
        cmdclass = cmdclass,
        options = options,
+       use_2to3 = True,
+       install_requires=[
+           'numpy>=1.6',
+           'oldnumeric',
+       ],
        )
