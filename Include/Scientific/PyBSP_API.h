bcd8e9922b0a5a30d9159ff43a6a03538c58d0d6
xoviat <xoviat@users.noreply.github.com>
2018-01-21 20:14:39 -0600

    Fix capsule checks

--- Include/Scientific/PyBSP_API.h
+++ Include/Scientific/PyBSP_API.h
@@ -142,8 +142,8 @@ static void **PyBSP_API;
   if (module != NULL) { \
     PyObject *module_dict = PyModule_GetDict(module); \
     PyObject *c_api_object = PyDict_GetItemString(module_dict, "_C_API"); \
-    if (PyCObject_Check(c_api_object)) { \
-      PyBSP_API = (void **)PyCObject_AsVoidPtr(c_api_object); \
+    if (PyCapsule_CheckExact(c_api_object)) { \
+      PyBSP_API = (void **)PyCapsule_GetPointer(c_api_object, NULL); \
     } \
   } \
 }
